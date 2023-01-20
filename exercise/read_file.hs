b26d81bd76a617c2e273aceacfe664e1df8d7ae9
filip <filip.naudot@gmail.com>
2023-01-20 22:29:17 +0000

    including line num in error msg and some cleaning

--- exercise/read_file.hs
+++ exercise/read_file.hs
@@ -2,45 +2,32 @@ import System.IO
 
 main = do
    contents <- readFile "input.txt"
-   let wordsList = words contents
-   mapM_ scan_token wordsList
-   --scan wordsList
+   let file_lines = lines contents
+   parse_line file_lines 1
 
 
--- scan_token - takes a token and scannes it
-scan_token token = do
-    putStr token -- Print the token
-    putStr " - " -- Print deliminator
+-- scan - takes a list of lines from a file and scannes them
+parse_line [] line_num = return () -- Check for empty list
+parse_line (line:lines) line_num = do
+    putStr ((show line_num) ++ ": ") -- print line number
+
+    let wordsList = words line
+    mapM_ (\n -> scan_token n line_num) wordsList
+
+    putStrLn "" -- new line
+    parse_line lines (line_num + 1)
+
+
+-- scan_token - takes a token and scans it
+scan_token token line_num = do
+    putStr (token ++ " ") -- Print the token
 
     if token == "test"
         then do
-            print_error ("Not allowed to write '" ++ token ++ "'. ")
-        else putStrLn "OK"
+            print_error ("Not allowed to write '" ++ token ++ "'. ") line_num
+        else return ()
 
 
--- print_error - prints a error message
-print_error error_message = do
-    putStrLn ("(ERROR) " ++ error_message)
-
-
-
-
-
-
--- scan - takes a list of tokes from a file
--- and scannes them
-scan [] = return () -- Check for empty list
-scan (token:tokens) = do
-    putStr token -- Print the token
-    putStr " - " -- Print deliminator
-
-    if token == "test" 
-        then do
-            let firstLetter = head token
-            putStr " ("
-            putChar firstLetter
-            putStr ") "
-            putStrLn "aha!"
-        else putStrLn "Not test"
-
-    scan tokens -- Next element in list
+-- print_error - prints an error message
+print_error error_message line_num = do
+    putStrLn ("\nError on line " ++ (show line_num) ++ ": " ++ error_message)
