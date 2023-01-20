7f9812ad08b2c9893de60128904cf8feecd5e780
filip <filip.naudot@gmail.com>
2023-01-20 15:15:52 +0000

    first letter

--- exercise/read_file.hs
+++ exercise/read_file.hs
@@ -3,14 +3,22 @@ import System.IO
 main = do
    contents <- readFile "input.txt"
    let wordsList = words contents
-   --mapM putStrLn wordsList
    scan wordsList
 
-scan [] = return ()
+-- This function takes a list of tokes from a file
+-- and scannes them
+scan [] = return () -- Check for empty list
 scan (token:tokens) = do
-    putStr token
-    putStr " - "
-    if token == "test"
-        then putStrLn "aha!"
+    putStr token -- Print the token
+    putStr " - " -- Print deliminator
+
+    if token == "test" 
+        then do
+            let firstLetter = head token
+            putStr " ("
+            putChar firstLetter
+            putStr ") "
+            putStrLn "aha!"
         else putStrLn "Not test"
-    scan tokens
+
+    scan tokens -- Next element in list
