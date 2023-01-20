28a28fb040f690a716ca1564a5cd0e2052c4aba0
filip <filip.naudot@gmail.com>
2023-01-20 21:13:37 +0000

    using mapM instead of recursion

--- exercise/read_file.hs
+++ exercise/read_file.hs
@@ -3,7 +3,25 @@ import System.IO
 main = do
    contents <- readFile "input.txt"
    let wordsList = words contents
-   scan wordsList
+   mapM scan_token wordsList
+   --scan wordsList
+
+
+-- This function takes a list of tokes from a file
+-- and scannes them
+scan_token token = do
+    putStr token -- Print the token
+    putStr " - " -- Print deliminator
+
+    if token == "test" 
+        then do
+            let firstLetter = head token
+            putStr " ("
+            putChar firstLetter
+            putStr ") "
+            putStrLn "aha!"
+        else putStrLn "Not test"
+
 
 -- This function takes a list of tokes from a file
 -- and scannes them
