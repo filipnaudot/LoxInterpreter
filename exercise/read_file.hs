7f874e562f2941508bfa360c230bb1c07b31c6ff
filip <filip.naudot@gmail.com>
2023-01-20 14:53:24 +0000

    pattern match string

--- exercise/read_file.hs
+++ exercise/read_file.hs
@@ -2,4 +2,15 @@ import System.IO
 
 main = do
    contents <- readFile "input.txt"
-   putStrLn contents
+   let wordsList = words contents
+   --mapM putStrLn wordsList
+   scan wordsList
+
+scan [] = return ()
+scan (token:tokens) = do
+    putStr token
+    putStr " - "
+    if token == "test"
+        then putStrLn "aha!"
+        else putStrLn "Not test"
+    scan tokens
