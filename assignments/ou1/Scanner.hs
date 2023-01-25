eda912b37a1c4041a730d9afccaac9ec917795b8
filip <filip.naudot@gmail.com>
2023-01-25 13:00:45 +0000

    generating some tokens (just experimenting)

--- assignments/ou1/Scanner.hs
+++ assignments/ou1/Scanner.hs
@@ -5,5 +5,28 @@ import Data.Char
 
 import Tokens
 
+import Debug.Trace
+
+-- Example of a token list with one token: [TOKEN LEFT_PAREN "test str" (ID "str_id") 1]
 scanTokens :: [Char] -> [Token]
-scanTokens str = [TOKEN LEFT_PAREN "test str" (ID "str_id") 1]
+scanTokens str =
+    trace "Start" $
+    let tokens = scan str 0
+    in trace "End" tokens
+
+scan :: [Char] -> Int -> [Token]
+scan [] _ = []
+scan (x:xs) lineNumber =
+    case x of
+        '(' -> (TOKEN LEFT_PAREN "(" NONE lineNumber) : scan xs (lineNumber + 1)
+        ')' -> (TOKEN RIGHT_PAREN ")" NONE lineNumber) : scan xs (lineNumber + 1)
+        '{' -> (TOKEN LEFT_BRACE "{" NONE lineNumber) : scan xs (lineNumber + 1)
+        '}' -> (TOKEN RIGHT_BRACE "}" NONE lineNumber) : scan xs (lineNumber + 1)
+        ',' -> (TOKEN COMMA "," NONE lineNumber) : scan xs (lineNumber + 1)
+        '.' -> (TOKEN DOT "." NONE lineNumber) : scan xs (lineNumber + 1)
+        '-' -> (TOKEN MINUS "-" NONE lineNumber) : scan xs (lineNumber + 1)
+        '+' -> (TOKEN PLUS "+" NONE lineNumber) : scan xs (lineNumber + 1)
+        ';' -> (TOKEN SEMICOLON ";" NONE lineNumber) : scan xs (lineNumber + 1)
+        '/' -> (TOKEN SLASH "/" NONE lineNumber) : scan xs (lineNumber + 1)
+        '*' -> (TOKEN STAR "*" NONE lineNumber) : scan xs (lineNumber + 1)
+        --'!' -> if (head xs) == '=' then (TOKEN BANG_EQUAL "!=" NONE lineNumber) : scan (tail xs) (lineNumber + 1) else (TOKEN BANG "!" NONE lineNumber)
